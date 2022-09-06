c4173684c2e62a1a6fe6abf0d0b032cfd2f9392d
Josh Muleshi <jmuleshi2@gmail.com>
2022-09-06 22:47:36 +0200

    commit again

--- TP2/main.c
+++ TP2/main.c
@@ -7,30 +7,44 @@ int main(){
     printf("avant: %d\n", liste.prLibre);
     //rendreCelluleA(&liste, liste.cle);
     //printf("apres: %d\n", liste.cle);
-    insereEnTete(&liste, 'G');
-    insereEnTete(&liste, 'X');
-    insereEnTete(&liste, 'Y');
-    insereApres(&liste, 'w');
+    insereApres(&liste, 'D');
+    insereApres(&liste, 'Y');
     insereEnTete(&liste, 'A');
+    insereApres(&liste, 'M');
 
-    afficherListe(&liste);
-    printf("\n\n");
+    suivant(&liste);
 
-    listSC b = tranStrToLSC("mamam");
+    insereApres(&liste, 'B');
+    insereEnTete(&liste, 'C');
+    insereEnTete(&liste, 'B');
+    
+    supprimeApres(&liste);
+    suivant(&liste);
+    suivant(&liste);
+    suivant(&liste);
+    supprimeApres(&liste);
+
+    //afficherListe(&liste);
+    //printf("\n\n");
+
+    /*listSC b = tranStrToLSC("mamam");
     afficherListe(&b);
     printf("\n\n");
 
     listSC a = supOcc("mamam", 'a');
     afficherListe(&a);
-    printf("\n\n");
+    printf("\n\n");*/
 
-    /*supprimeEnTete(&liste);
+    //supprimeEnTete(&liste);
     
     printf("taille : %d\n", liste.taille);
     printf("premier : %d\n", liste.premier);
     printf("prLibre : %d\n", liste.prLibre);
-    
+    printf("cle : %d\n", liste.cle);
+    //int i = liste.tab[liste.premier].val;
     printf("val : %c\n", liste.tab[liste.premier].val);
+    
+    /*printf("val : %c\n", liste.tab[liste.premier].val);
     int i = liste.tab[liste.premier].idSuiv;
     printf("val : %c\n", liste.tab[i].val);
     printf("cle : %d\n", liste.cle);*/
--- TP5/main.c
+++ TP5/main.c
@@ -2,16 +2,23 @@
 
 int main(){
     TasMin t;
-    creerTas(&t, 'D');
-    entasser(&t, 'C');
-    entasser(&t, 'B');
-    entasser(&t, 'A');
-    entasser(&t, 'G');
-    entasser(&t, 'F');
-    entasser(&t, 'E');
+    creerTas(&t, '2');
+    entasser(&t, '5');
+    entasser(&t, '7');
+    entasser(&t, '9');
+    entasser(&t, '3');
+    printf("%c\n\n", valeur(&t));
+    printf("fils gauche : %c\n", filsG(&t, RACINE));
+    printf("fils Droit : %c\n", filsD(&t, RACINE));
+    printf("fils gauche : %c\n", filsG(&t, RACINE * 2));
+    printf("fils Droit : %c\n\n", filsD(&t, RACINE * 2));
+    entasser(&t, '8');
+    entasser(&t, '4');
+    entasser(&t, '1');
+    entasser(&t, '6');
 
-    supprimer(&t);
-    supprimer(&t);
+    //supprimer(&t);
+    //supprimer(&t);
     
     printf("%c\n\n", valeur(&t));
     printf("fils gauche : %c\n", filsG(&t, RACINE));
